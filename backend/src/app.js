34e1fc535227299cb48a844304df65d5bd46e5fa
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-06-06 20:19:25 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const notesmodel = require('./modals/notes.modals');
+const cors = require('cors');
+
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.post('/api/notes' , async (req, res) => {
+
+  const {title, description, age} = req.body;
+
+  const note = await notesmodel.create({
+    title , description , age
+  })
+
+  res.status(201).json({
+    message : 'Note created successfully',
+    note
+  })
+})
+
+
+app.get('/api/notes' , async (req, res) => {
+
+  const notes = await notesmodel.find();
+
+  res.status(200).json({
+    message : 'Notes fetched successfully',
+    notes
+  })
+})
+
+app.delete('/api/notes/:id' , async (req, res) => {
+
+  const {id} = req.params;
+  await notesmodel.findByIdAndDelete(id);
+
+  res.status(200).json({
+    message : 'Note deleted successfully',
+  })
+})
+
+app.patch('/api/notes/:id' , async (req, res) => {
+
+  const id = req.params.id;
+
+  const {description} = req.body;
+  const notes =  await notesmodel.findByIdAndUpdate(id, {description});
+
+
+
+  res.status(200).json({
+    message : 'Note updated successfully',
+    
+  })
+})
+
+
+module.exports = app;
